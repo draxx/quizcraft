6887e3cf5e2f9cb46d4dd6b418d766ad40b1d705
kevin <kloiseau@gmail.com>
2025-07-08 09:24:09 +0200

    feat(api): add fetchQuestionById and updateQuestion functions

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,3 +1,5 @@
+import type { QuestionFormFields } from "@/types/question";
+
 export async function fetchQuestions() {
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/questions`, {
     headers: {
@@ -5,6 +7,33 @@ export async function fetchQuestions() {
     },
   });
 
-  if (!res.ok) throw new Error("Erreur lors du chargement des questions");
+  if (!res.ok) throw new Error("Impossible de charger les questions !");
+  return res.json();
+}
+
+export async function fetchQuestionById(id:string) {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/questions/${id}`, {
+    headers: {
+      "x-api-key": process.env.NEXT_PUBLIC_API_KEY!,
+    },
+  });
+
+  if (!res.ok) throw new Error("Impossible de charger la question !");
+  return res.json();
+}
+
+export async function updateQuestion(id: string, data: QuestionFormFields) {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/questions/${id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      "x-api-key": process.env.NEXT_PUBLIC_API_KEY!,
+    },
+    body: JSON.stringify(data),
+  });
+
+  if (!res.ok) {
+    throw new Error(`Erreur lors de la mise à jour: ${res.statusText}`);
+  }
   return res.json();
 }
